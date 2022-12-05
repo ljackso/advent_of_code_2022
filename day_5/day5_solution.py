15497bbce4f298f61d9d41b021456b4d8d467f19
Luke Jackson <ljackso.93@gmail.com>
2022-12-05 14:51:41 +0100

    Update day5_solution.py

--- day_5/day5_solution.py
+++ day_5/day5_solution.py
@@ -4,21 +4,21 @@ def main():
     file = open(input_file_name).read().splitlines()
 
     stack = []
-    end = 0
+    end_diagram = 0
     for index, line in enumerate(file):
         if line == '':
             for i in range(0,int(file[index-1][-1])):
                 stack.append([])
-                end = index - 2
+                end_diagram = index - 2
             break
 
     entry_len = (len(stack) * 4) - 1
     for index, line in enumerate(file):
-        if index <= end:
+        if index <= end_diagram:
             for i in range(0,len(line)):
                 if not (i - 1) % 4 and line[i] != ' ':
                     stack[(i-1)//4].insert(0,line[i])
-        elif index > (end + 2):
+        elif index > (end_diagram + 2):
             instr = line.split(' ')
             q, pos1, pos2 = int(instr[1]),int(instr[3])-1,int(instr[5])-1
             temp_stack = stack[pos1][-q:]
